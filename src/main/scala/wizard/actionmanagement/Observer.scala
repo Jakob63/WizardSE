6c48caedb2f914e7c9c84619596034cd809b2da5
Jakob63 <jakobzim20002@gmail.com>
2024-11-22 00:41:39 +0100

    tests updated TestSequence added

--- src/main/scala/wizard/actionmanagement/Observer.scala
+++ src/main/scala/wizard/actionmanagement/Observer.scala
@@ -7,6 +7,6 @@ trait Observer {
 class Observable {
     var subscribers:Vector[Observer] = Vector()
     def add(s:Observer): Unit = subscribers=subscribers:+s
-    //def remove(s:Observer): Unit = subscribers=subscribers.filterNot(o=>o==s)
+    //def remove(s:Observer): Unit = subscribers=subscribers.filterNot(o=>o==s) // wofür bräuchte man das
     def notifyObservers(updateMSG: String, obj: Any*): Unit = subscribers.foreach(o=>o.update(updateMSG, obj*))
 }
