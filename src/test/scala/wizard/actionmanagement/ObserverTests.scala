5e6e1f36f3a6bc2c53e96e450c3f049cdaecd903
Jakob63 <jakobzim20002@gmail.com>
2024-11-27 12:21:07 +0100

    Tests update

--- src/test/scala/wizard/actionmanagement/ObserverTests.scala
+++ src/test/scala/wizard/actionmanagement/ObserverTests.scala
@@ -7,6 +7,14 @@ import org.scalatest.wordspec.AnyWordSpec
 class ObserverTests extends AnyWordSpec with Matchers {
     "Observer" should {
 
+        "should add observer" in {
+            val observable = new Observable()
+            val observer = new Observer() {
+                override def update(updateMSG: String, obj: Any*): Unit = {}
+            }
+            observable.add(observer)
+            observable.subscribers should contain(observer)
+        }
         "should remove observer" in {
             val observable = new Observable()
             val observer = new Observer(){
@@ -16,5 +24,18 @@ class ObserverTests extends AnyWordSpec with Matchers {
             observable.remove(observer)
             observable.subscribers should not contain observer
         }
+        "should notify observer" in {
+            val observable = new Observable()
+            var notified = false
+            val observer = new Observer(){
+                override def update(updateMSG: String, obj: Any*): Unit = {
+                    notified = true
+                }
+            }
+            observable.add(observer)
+            observable.notifyObservers("test")
+            notified shouldBe true
+        }
+
     }
 }
