92386c3e908086b5ed0d10a199c6a94717f6208a
jakob63 <jakobzim20002@gmail.com>
2025-10-21 14:21:19 +0200

    Base Logic

--- src/main/scala/wizard/controller/controllerBaseImpl/BaseGameLogic.scala
+++ src/main/scala/wizard/controller/controllerBaseImpl/BaseGameLogic.scala
@@ -1,5 +1,56 @@
 package wizard.controller.controllerBaseImpl
 
-class BaseGameLogic {
+import wizard.actionmanagement.Observable
+import wizard.controller.{aGameLogic, aRoundLogic}
+import wizard.model.player.Player
+import wizard.model.rounds.{Game, Round}
 
+class BaseGameLogic extends Observable with aGameLogic{
+  
+  
+  
+  // Logics
+  val roundLogic: aRoundLogic = new BaseRoundLogic()
+  
+  
+  
+  override def startGame() = {
+    notifyObservers("main menu")
+  }
+
+  override def askPlayerNumber(): Unit = {
+    notifyObservers("input players")
+  }
+
+  override def createGame(players: List[Player]) = {
+    val game = Game(players)
+    notifyObservers("game started")
+    playGame(game, players)
+  }
+
+  override def createPlayers(numPlayers: Int, current: Int = 0, players: List[Player] = List()) = {
+    if (current < numPlayers) {
+      notifyObservers("player names", numPlayers, current, players)
+    } else {
+      createGame(players)
+    }
+
+  }
+
+  override def validGame(number: Int): Boolean = {
+    number >= 3 && number <= 6
+  }
+
+  override def playGame(game: Game, players: List[Player]): Unit = {
+    for (i <- 1 to game.rounds) { // i = 1, 2, 3, ..., rounds
+      game.currentround = i
+      val round = new Round(players)
+      roundLogic.playRound(game.currentround, players)
+    }
+  }
+
+  // game is over if all rounds are played
+  override def isOver(game: Game): Boolean = {
+    game.rounds == 0
+  }
 }
--- src/main/scala/wizard/controller/controllerBaseImpl/BaseRoundLogic.scala
+++ src/main/scala/wizard/controller/controllerBaseImpl/BaseRoundLogic.scala
@@ -1,4 +1,90 @@
-package wizard.controller.controllerBaseImpl;
+package wizard.controller.controllerBaseImpl
 
-class BaseRoundLogic {
+import wizard.aView.TextUI.showHand
+import wizard.actionmanagement.Observable
+import wizard.controller.{aPlayerLogic, aRoundLogic}
+import wizard.model.cards.{Card, Dealer, Value}
+import wizard.model.player.Player
+import wizard.model.rounds.Round;
+
+class BaseRoundLogic extends Observable with aRoundLogic {
+  
+  // Logics
+  val playerLogic: aPlayerLogic = BasePlayerLogic()
+  
+  
+
+  def playRound(currentround: Int, players: List[Player]): Unit = {
+    val round = new Round(players)
+    val trumpCardIndex = currentround * players.length
+    val trumpCard = if (trumpCardIndex < Dealer.allCards.length) {
+      Dealer.allCards(trumpCardIndex)
+    } else {
+      throw new IndexOutOfBoundsException("No trump card available.")
+    }
+    round.setTrump(trumpCard.color)
+    notifyObservers("print trump card", trumpCard)
+
+    Dealer.shuffleCards()
+    players.foreach { player =>
+      val hand = Dealer.dealCards(currentround, Some(trumpCard))
+      player.addHand(hand)
+    }
+    notifyObservers("cards dealt")
+    players.foreach(showHand)
+
+    players.foreach(player => playerLogic.bid(player))
+    for (i <- 1 to currentround) {
+      round.leadColor = None
+      var trick = List[(Player, Card)]()
+      var firstPlayerIndex = 0
+
+      while (round.leadColor.isEmpty && firstPlayerIndex < players.length) {
+        val player = players(firstPlayerIndex)
+        val card = playerLogic.playCard(null, round.trump, firstPlayerIndex, player)
+        if (card.value != Value.WizardKarte && card.value != Value.Chester) {
+          round.leadColor = Some(card.color)
+        }
+        trick = trick :+ (player, card)
+        firstPlayerIndex += 1
+      }
+
+      for (j <- firstPlayerIndex until players.length) {
+        val player = players(j)
+        val card = playerLogic.playCard(round.leadColor.getOrElse(null), round.trump, j, player)
+        trick = trick :+ (player, card)
+      }
+
+      trick.foreach { case (player, _) =>
+        if (!player.hand.isEmpty) {
+          showHand(player)
+        }
+      }
+      val winner = trickwinner(trick, round)
+      notifyObservers("trick winner", winner)
+      winner.roundTricks += 1
+    }
+
+    players.foreach(player => {
+      player.addTricks(player.roundTricks)
+    })
+    players.foreach(player => {
+      playerLogic.addPoints(player)
+    })
+    notifyObservers("points after round")
+    notifyObservers("print points all players", players)
+  }
+
+  def trickwinner(trick: List[(Player, Card)], round: Round): Player = {
+    val leadColor = trick.head._2.color
+    val trump = round.trump
+    val leadColorCards = trick.filter(_._2.color == leadColor)
+    val trumpCards = trick.filter(_._2.color == trump)
+    val winningCard = if (trumpCards.nonEmpty) {
+      trumpCards.maxBy(_._2.value.ordinal)
+    } else {
+      leadColorCards.maxBy(_._2.value.ordinal)
+    }
+    winningCard._1
+  }
 }
