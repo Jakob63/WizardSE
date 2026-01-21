3f0df6eda475abe0f6d4645aeefe5a3226ac07f6
el871bra <el871bra@htwg-konstanz.de>
2026-01-21 13:39:57 +0100

    fix comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-# Start Xvfb
-# virtuelles Display :99
-Xvfb :99 -screen 0 1024x768x24 & # Auflösung 1024x768
+Xvfb :99 -screen 0 1024x768x24 &
 export DISPLAY=:99
 
 sleep 2
 
-# Window Manager für FX-Apps (ist besser)
+fluxbox &
 sleep 2
 
-# Start x11vnc, vDisplay mit VNC freigeben
-# -forever: nach Trennung offen
-# -shared: mehrere Connections
-# -nopw: Kein Passwort
+# vDisplay mit VNC freigeben
 x11vnc -display :99 -forever -shared -nopw -quiet &
 
-# Start noVNC Proxy um VNC mit Web-Sockets frei machen
-# Port 6080 = Standard für noVNC
+# noVNC Proxy um VNC mit Web-Sockets frei machen
 /usr/share/novnc/utils/launch.sh --vnc localhost:5900 --listen 6080 &
 
-# Start von Wizard
 java -Djava.awt.headless=false -jar app.jar
