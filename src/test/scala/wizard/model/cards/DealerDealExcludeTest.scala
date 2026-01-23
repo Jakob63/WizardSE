e62d33a39a20cd3611b87412ab4f4a71da453e0e
jakob63 <jakobzim20002@gmail.com>
2026-01-23 09:04:09 +0100

    fix Tui Tests

--- src/test/scala/wizard/model/cards/DealerDealExcludeTest.scala
+++ src/test/scala/wizard/model/cards/DealerDealExcludeTest.scala
@@ -5,7 +5,6 @@ import org.scalatest.matchers.should.Matchers
 
 class DealerDealExcludeTest extends AnyFunSuite with Matchers {
   test("Dealer has 60 cards initially and dealCards returns requested amount") {
-    // 15 values * 4 colors = 60
     Dealer.index = 0
     Dealer.allCards.size should be (15 * Color.values.size)
     val hand = Dealer.dealCards(5, None)
@@ -16,7 +15,6 @@ class DealerDealExcludeTest extends AnyFunSuite with Matchers {
     Dealer.index = 0
     val cardToExclude = Dealer.allCards.head
     val hand = Dealer.dealCards(1, Some(cardToExclude))
-    // ensure the dealt card is not the excluded one
     hand.cards.head should not be (cardToExclude)
   }
 }
